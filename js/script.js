b315329784e67fa77f40edbca216543d14d67314
Dany <profedanieljuan@gmail.com>
2024-06-19 21:09:35 -0300

    Primer ejemplo de javascript capturando los datos de la pagina con javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// alert("Hola")
+
+function sumar(){
+    a=document.getElementById("a").value
+    b=document.getElementById("b").value
+
+    resultado=parseInt(a)+parseInt(b)
+
+    document.getElementById("result").innerHTML(resultado)
+}
