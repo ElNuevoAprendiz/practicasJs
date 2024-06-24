d12f31d14f3e227a996596479ce4c106a07d6fb6
Dany <profedanieljuan@gmail.com>
2024-06-24 10:30:22 -0300

    En el codigo del fortaleza habia varios errores, en el javascript donde se uso en eldentro del if =< en vez de <= y por otro lado en el getElementbyId se le agrego una s a Element, quedando getElementsById y de ahi no funcionaba el codigo.

--- fortaleza.js
+++ fortaleza.js
@@ -1,19 +1,21 @@
 function calcularFortaleza(){
 
+    
     let contrasenia=document.getElementById("pass").value
-    alert(contrasenia)
+   
 
     cantCaracteres=contrasenia.length
 
-    alert(contrasenia)
+    
 
-    if (cantCaracteres>=0 & cantCaracteres=<4) {
-        fort="Baja"
-        
-    } else if (cantCaracteres>=5 & cantCaracteres=<8) {
-        fort="Media"
+    if (cantCaracteres>=0 & cantCaracteres<=4) {
+        fort="Baja";
+        alert("Baja");
+    } else if (cantCaracteres>=5 & cantCaracteres<=8) {
+        fort="Media";
     } else {
-        fort="Alta"
+        fort="Alta";
     }
-    document.getElementsById("fort").innerHTML = fort
+
+    document.getElementById("fort").innerHTML = fort
 }
